98a5990ce634eb9623bea4d34e11822d2e2107ed
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-10-04 16:41:03 +0900

    Update isPrime

--- Number.scala
+++ Number.scala
@@ -7,15 +7,17 @@ object Number {
   def rand(min: Int, max: Int): Int = scala.util.Random.nextInt(max - min) + min
 
   def isPrime(N: Int): Boolean = {
-    if (N == 2) true
-    else if (N < 2 || N % 2 == 0) false
-    else if (
+    def isEmpty(N: Int): Boolean = {
       LazyList
         .from(3, 2)
         .take(scala.math.sqrt(N).toInt)
         .find(N % _ == 0)
         .isEmpty
-    ) true
+    }
+
+    if (N == 2) true
+    else if (N < 2 || N % 2 == 0) false
+    else if (isEmpty(N)) true
     else false
   }
 
