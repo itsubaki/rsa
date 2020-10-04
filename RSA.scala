f1c61970115fb5058a768ed824f6e9bbd4ca1223
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-10-04 10:22:01 +0900

    Update mod

--- RSA.scala
+++ RSA.scala
@@ -2,13 +2,10 @@ object RSA {
   def findD(phi: Int, e: Int): Int =
     LazyList.from(1).take(phi).find(d => (e * d) % phi == 1).get
 
-  def encrypt(message: Int, e: Int, n: Int): Int = {
-    @scala.annotation.tailrec
-    def mod(message: Int, e: Int, n: Int, acc: Int): Int =
-      if (e == 1) acc else mod(message, e - 1, n, (acc * message) % n)
+  @scala.annotation.tailrec
+  def mod(message: Int, e: Int, n: Int, acc: Int): Int =
+    if (e == 1) acc else mod(message, e - 1, n, (acc * message) % n)
 
-    return mod(message, e, n, message)
-  }
-
-  def decrypt(message: Int, d: Int, n: Int): Int = encrypt(message, d, n)
+  def encrypt(message: Int, e: Int, n: Int): Int = mod(message, e, n, message)
+  def decrypt(message: Int, d: Int, n: Int): Int = mod(message, d, n, message)
 }
