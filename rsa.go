4e3357c216ab1101ff0ada479129c98e2921e6e5
itsubaki <1759459+itsubaki@users.noreply.github.com>
2024-05-06 01:09:34 +0000

    Add godoc

--- rsa.go
+++ rsa.go
@@ -2,35 +2,40 @@ package rsa
 
 import "github.com/itsubaki/rsa/number"
 
+// PublicKey returns a public key.
 func PublicKey(e int, seed ...[32]byte) int {
 	return Must(number.Coprime(e, seed...))
 }
 
+// PrivateKey returns a private key.
 func PrivateKey(e, E int) int {
-	for i := 1; ; i++ {
-		if (E*i)%e == 1 {
-			return i
+	for x := 1; ; x++ {
+		if (E*x)%e == 1 {
+			return x
 		}
 	}
 }
 
+// Encrypt returns an encrypted message.
 func Encrypt(msg, E, N int) int {
 	return mod(msg, E, N)
 }
 
+// Decrypt returns a decrypted message.
 func Decrypt(msg, D, N int) int {
 	return mod(msg, D, N)
 }
 
 func mod(msg int, e int, n int) int {
-	out := msg
+	x := msg
 	for i := 1; i < e; i++ {
-		out = (out * msg) % n
+		x = (x * msg) % n
 	}
 
-	return out
+	return x
 }
 
+// Must panics if err is not nil.
 func Must[T any](a T, err error) T {
 	if err != nil {
 		panic(err)
